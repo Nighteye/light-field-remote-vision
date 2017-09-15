bb6ec5372c0e8e6d6e9e43e18414ed54117288b6
gnietol <gregoire.nieto@inria.fr>
2017-09-15 15:50:20 +0200

    remove previous video when saving new one

--- transcut.sh
+++ transcut.sh
@@ -15,8 +15,11 @@ for i in {1..7}
 do
 	for j in {1..7}
 	do
+		rm 'out/IBR_optical/transcut/obj'$i'_scn'$j'/panning3param_02_02.mp4'
 		ffmpeg -r 60 -f image2 -s 629x469 -i 'out/IBR_optical/transcut/obj'$i'_scn'$j'/panning3param_02_02_%03d.png' -vcodec libx264 -crf 15 'out/IBR_optical/transcut/obj'$i'_scn'$j'/panning3param_02_02.mp4'
+		rm 'out/IBR_optical/transcut/obj'$i'_scn'$j'/panning4param_02_02.mp4'
 		ffmpeg -r 60 -f image2 -s 629x469 -i 'out/IBR_optical/transcut/obj'$i'_scn'$j'/panning4param_02_02_%03d.png' -vcodec libx264 -crf 15 'out/IBR_optical/transcut/obj'$i'_scn'$j'/panning4param_02_02.mp4'
+		rm 'out/IBR_optical/transcut/obj'$i'_scn'$j'/panning6param_02_02.mp4'
 		ffmpeg -r 60 -f image2 -s 629x469 -i 'out/IBR_optical/transcut/obj'$i'_scn'$j'/panning6param_02_02_%03d.png' -vcodec libx264 -crf 15 'out/IBR_optical/transcut/obj'$i'_scn'$j'/panning6param_02_02.mp4'		
 	done
 done
