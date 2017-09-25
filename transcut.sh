1b2f16b764ca4a6a084467defa8a3a34d7178f33
gnietol <gregoire.nieto@inria.fr>
2017-09-25 10:48:25 +0200

    d instead of lu

--- transcut.sh
+++ transcut.sh
@@ -4,20 +4,20 @@
 
 # ESTIMATE MODELS AND RENDER NOVEL VIEWS
 
-for i in {1..7}
-do
-	for j in {1..7}
-	do
+#for i in {1..7}
+#do
+#	for j in {1..7}
+#	do
 #		./ULF -config 'configs/IBR_optical/transcut/obj'$i'scn'$j'view12'
-		./ULF -config 'configs/IBR_optical/transcut/obj'$i'scn'$j'view04'
-		./ULF -config 'configs/IBR_optical/transcut/obj'$i'scn'$j'view22'
-		./ULF -config 'configs/IBR_optical/transcut/obj'$i'scn'$j'allViews'
-	done
-done
+#		./ULF -config 'configs/IBR_optical/transcut/obj'$i'scn'$j'view04'
+#		./ULF -config 'configs/IBR_optical/transcut/obj'$i'scn'$j'view22'
+#		./ULF -config 'configs/IBR_optical/transcut/obj'$i'scn'$j'allViews'
+#	done
+#done
 
 # UNIT TEST
 
-./ULF -config configs/IBR_optical/unitTest
+#./ULF -config configs/IBR_optical/unitTest
 
 # CONVERT IMAGE SEQUENCE TO VIDEO
 
@@ -26,11 +26,11 @@ do
 	for j in {1..7}
 	do
 		rm 'out/IBR_optical/transcut/obj'$i'_scn'$j'/3g_IBR_panning.mp4'
-		ffmpeg -r 60 -f image2 -s 629x469 -i 'out/IBR_optical/transcut/obj'$i'_scn'$j'/3g_IBR_panning_%03lu.png' -vcodec libx264 -crf 15 'out/IBR_optical/transcut/obj'$i'_scn'$j'/3g_IBR_panning.mp4'
+		ffmpeg -r 60 -f image2 -s 629x469 -i 'out/IBR_optical/transcut/obj'$i'_scn'$j'/3g_IBR_panning_%03d.png' -vcodec libx264 -crf 15 'out/IBR_optical/transcut/obj'$i'_scn'$j'/3g_IBR_panning.mp4'
 		rm 'out/IBR_optical/transcut/obj'$i'_scn'$j'/4g_IBR_panning.mp4'
-		ffmpeg -r 60 -f image2 -s 629x469 -i 'out/IBR_optical/transcut/obj'$i'_scn'$j'/4g_IBR_panning_%03lu.png' -vcodec libx264 -crf 15 'out/IBR_optical/transcut/obj'$i'_scn'$j'/4g_IBR_panning.mp4'
+		ffmpeg -r 60 -f image2 -s 629x469 -i 'out/IBR_optical/transcut/obj'$i'_scn'$j'/4g_IBR_panning_%03d.png' -vcodec libx264 -crf 15 'out/IBR_optical/transcut/obj'$i'_scn'$j'/4g_IBR_panning.mp4'
 		rm 'out/IBR_optical/transcut/obj'$i'_scn'$j'/6g_IBR_panning.mp4'
-		ffmpeg -r 60 -f image2 -s 629x469 -i 'out/IBR_optical/transcut/obj'$i'_scn'$j'/6g_IBR_panning_%03lu.png' -vcodec libx264 -crf 15 'out/IBR_optical/transcut/obj'$i'_scn'$j'/6g_IBR_panning.mp4'		
+		ffmpeg -r 60 -f image2 -s 629x469 -i 'out/IBR_optical/transcut/obj'$i'_scn'$j'/6g_IBR_panning_%03d.png' -vcodec libx264 -crf 15 'out/IBR_optical/transcut/obj'$i'_scn'$j'/6g_IBR_panning.mp4'		
 	done
 done
 
