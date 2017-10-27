e505956ae22c747be57d97f290075b1eb16d28fd
Gregoire Nieto <nietog@renndw7rdl4333.rennes.eu.thmulti.com>
2017-10-27 17:29:10 +0200

    update unittest

--- unitTest/unitTest.sh
+++ unitTest/unitTest.sh
@@ -1,5 +1,6 @@
 # CREATE reference data from transcut/obj1_scn1
 
+#rm ../out/IBR_optical/unitTest/
 #cp ../out/IBR_optical/transcut/obj1_scn1/* ../out/IBR_optical/unitTest/
 #cd ../out/IBR_optical/unitTest
 #for f in *
@@ -10,9 +11,9 @@
 
 # RUN tests
 
-#cd ..
-#./ULF -config configs/IBR_optical/unitTest
-#cd unitTest
+cd ..
+./ULF -config configs/IBR_optical/unitTest
+cd unitTest
 
 # COMPARE with reference
 
